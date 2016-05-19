678fd628b0a29e13fdd3885120197f2b8cc8c918
xcs <13162927913@163.com>
2016-05-19 09:12:27 +0800

    Init

---
+++ MovieIndex/MovieIndex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovieIndex
+//
+//  Created by 谢朝森 on 16/5/19.
+//  Copyright (c) 2016年 xcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MovieIndex/MovieIndex/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MovieIndex
+//
+//  Created by 谢朝森 on 16/5/19.
+//  Copyright (c) 2016年 xcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
